62375a28578394eb43b8f9eb79b6fb6e753a814f
nobusugi246 <nobusugi246@gmail.com>
2020-10-18 13:29:51 +0900

    refactor to use delete func.

--- operator.py
+++ operator.py
@@ -1,64 +1,59 @@
 import kopf
 import subprocess
-from pykube import Deployment, HTTPClient, KubeConfig
+from pykube import Service, Deployment, HTTPClient, KubeConfig, ObjectDoesNotExist
+
+
+def delete(namespace, names, logger):
+    api = HTTPClient(KubeConfig.from_file())
+    for name in names:
+        deploy = Deployment.objects(api, namespace=namespace).get(name=name)
+        deploy.delete()
+        logger.info(f'delete Deployment: {str(deploy)}')
+        service = Service.objects(api, namespace=namespace).get(name=name)
+        service.delete()
+        logger.info(f'delete Service:    {str(service)}')
 
 
 @kopf.on.field('tekton.dev', 'v1beta', 'companions', field='spec.pipeline.version')
-def pipeline(old, new, logger, **kwargs):
+def pipeline(spec, old, new, logger, **kwargs):
     logger.info(f'pipeline: {old=}, {new=}')
     if new:
         subprocess.run(f"kubectl apply -f https://github.com/tektoncd/pipeline/releases/download/{new}/release.yaml", shell=True, check=True)
-        
+    elif old:
+        delete(spec.get('namespace', 'tekton-pipelines'),
+               ["tekton-pipelines-controller", "tekton-pipelines-webhook"],
+               logger)
+
 
 @kopf.on.field('tekton.dev', 'v1beta', 'companions', field='spec.triggers.version')
-def triggers(old, new, logger, **kwargs):
+def triggers(spec, old, new, logger, **kwargs):
     logger.info(f'triggers: {old=}, {new=}')
     if new:
         subprocess.run(f"kubectl apply -f https://github.com/tektoncd/triggers/releases/download/{new}/release.yaml", shell=True, check=True)
+    elif old:
+        delete(spec.get('namespace', 'tekton-pipelines'),
+               ["tekton-triggers-controller", "tekton-triggers-webhook"],
+               logger)
 
 
 @kopf.on.field('tekton.dev', 'v1beta', 'companions', field='spec.dashboard.version')
-def dashboard(old, new, logger, **kwargs):
+def dashboard(spec, old, new, logger, **kwargs):
     logger.info(f'dashboard: {old=}, {new=}')
     if new:
         subprocess.run(f"kubectl apply -f https://github.com/tektoncd/dashboard/releases/download/{new}/tekton-dashboard-release.yaml", shell=True, check=True)
+    elif old:
+        delete(spec.get('namespace', 'tekton-pipelines'),
+               ["tekton-dashboard"],
+               logger)
 
 
 @kopf.on.delete('tekton.dev', 'v1beta', 'companions')
 def uninstall(spec, logger, **kwargs):
     logger.info('uninstall')
-    api = HTTPClient(KubeConfig.from_file())
     try:
-        deploy = Deployment.objects(api, namespace=spec.get('namespace', 'tekton-pipelines')).get(name="tekton-pipelines-controller")
-        logger.info(f'undeploy {str(deploy)}')
-        deploy.delete()
-    except pykube.exceptions.ObjectDoesNotExist:
-        pass
-
-    try:
-        deploy = Deployment.objects(api, namespace=spec.get('namespace', 'tekton-pipelines')).get(name="tekton-pipelines-webhook")
-        logger.info(f'undeploy {str(deploy)}')
-        deploy.delete()
-    except pykube.exceptions.ObjectDoesNotExist:
-        pass
-
-    try:
-        deploy = Deployment.objects(api, namespace=spec.get('namespace', 'tekton-pipelines')).get(name="tekton-triggers-controller")
-        logger.info(f'undeploy {str(deploy)}')
-        deploy.delete()
-    except pykube.exceptions.ObjectDoesNotExist:
-        pass
-
-    try:
-        deploy = Deployment.objects(api, namespace=spec.get('namespace', 'tekton-pipelines')).get(name="tekton-triggers-webhook")
-        logger.info(f'undeploy {str(deploy)}')
-        deploy.delete()
-    except pykube.exceptions.ObjectDoesNotExist:
-        pass
-
-    try:
-        deploy = Deployment.objects(api, namespace=spec.get('namespace', 'tekton-pipelines')).get(name="tekton-dashboard")
-        logger.info(f'undeploy {str(deploy)}')
-        deploy.delete()
-    except pykube.exceptions.ObjectDoesNotExist:
+        delete(spec.get('namespace', 'tekton-pipelines'), 
+               ["tekton-pipelines-controller", "tekton-pipelines-webhook",
+                "tekton-triggers-controller", "tekton-triggers-webhook", "tekton-dashboard"],
+               logger)
+    except ObjectDoesNotExist:
         pass
